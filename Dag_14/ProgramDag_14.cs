d54f71355d678f9ed70be81d0f6b6f011ec5dfaf
Dutchpainter <yolanda.ter.horst@xs4all.nl>
2021-02-04 17:24:48 +0100

    fix

---
+++ Dag_14/ProgramDag_14.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dag_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
